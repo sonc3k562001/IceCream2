53b2c81b45eb2aed22fd2167a5b3606c86e3104b
sonc3k562001 <54662662+sonc3k562001@users.noreply.github.com>
2020-12-15 17:54:00 +0700

    ok
    ok

---
+++ Ice-Cream-master/Ice_Cream/Models/StoreDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ice_Cream.Models
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options): base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        
+    }
+}
