f90706c8ec815c4476337bd6674b717fe200d112
an <54616933+tranthian-snake@users.noreply.github.com>
2020-12-23 17:50:39 +0700

    Home/Index thêm đg link Login, site.css phần login, LoginController, login.css, Login/(Index,SignUp, PasswordReset, PasswordChange)

---
+++ Ice-Cream-master/Ice_Cream/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ice_Cream.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        public ActionResult PasswordReset()
+        {
+            return View();
+        }
+        public ActionResult PasswordChange()
+        {
+            return View();
+        }
+    }
+}
