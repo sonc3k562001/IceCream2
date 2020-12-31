5043b0456a3b9c06338521b3a1edab0e17fe24ef
an <54616933+tranthian-snake@users.noreply.github.com>
2020-12-31 15:55:41 +0700

    ProviderController, provider.cs, allorder.cs, tệp Provider

--- Ice-Cream-master/Ice_Cream/Controllers/ProviderController.cs
+++ Ice-Cream-master/Ice_Cream/Controllers/ProviderController.cs
@@ -12,5 +12,62 @@ namespace Ice_Cream.Controllers
         {
             return View();
         }
+        public IActionResult All_order()
+        {
+            return View();
+        }
+
+        public IActionResult All_product()
+        {
+            return View();
+        }
+        public IActionResult Recipe_free()
+        {
+            return View();
+        }
+        public IActionResult Recipe_fee()
+        {
+            return View();
+        }
+        public IActionResult Recipe_bundle()
+        {
+            return View();
+        }
+        public IActionResult Sales()
+        {
+            return View();
+        }
+        public IActionResult Bank_account()
+        {
+            return View();
+        }
+        public IActionResult Payment()
+        {
+            return View();
+        }
+        public IActionResult Sale_analysis()
+        {
+            return View();
+        }
+        public IActionResult Effect()
+        {
+            return View();
+        }
+        public IActionResult Chat_assitant()
+        {
+            return View();
+        }
+        public IActionResult File_shop()
+        {
+            return View();
+        }
+        public IActionResult Category_shop()
+        {
+            return View();
+        }
+        public IActionResult MyReport()
+        {
+            return View();
+        }
     }
 }
